13b0456776cca24aafa2fdd9626cc598ab5da7be
Jijesh Mohan <jijeshmohan@gmail.com>
2013-10-20 20:09:25 +0530

    refectored crawling logic

---
+++ crawl.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/PuerkitoBio/gocrawl"
+	"github.com/PuerkitoBio/goquery"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+	"time"
+)
+
+type CrawlerExtender struct {
+	gocrawl.DefaultExtender
+	Section string
+	outDir  string
+}
+
+func (this *CrawlerExtender) Visit(ctx *gocrawl.URLContext, res *http.Response, doc *goquery.Document) (interface{}, bool) {
+	title := strings.TrimSpace(doc.Find("title").Text())
+
+	if doc.Find(this.Section).Length() == 0 {
+		return nil, true
+	}
+
+	f, _ := os.Create(this.outDir + "/" + title + ".txt")
+	defer f.Close()
+
+	body := doc.Find(this.Section).Text()
+	f.WriteString(body)
+
+	return nil, true
+}
+
+func crawlSite(siteConfig SiteConfig, wg *sync.WaitGroup) {
+	defer wg.Done()
+	crawler := new(CrawlerExtender)
+	crawler.Section = siteConfig.Section
+	crawler.outDir = *outDir
+
+	opts := gocrawl.NewOptions(crawler)
+	opts.CrawlDelay = 1 * time.Second
+
+	opts.MaxVisits = siteConfig.Depth
+
+	c := gocrawl.NewCrawlerWithOptions(opts)
+	c.Run(siteConfig.Url)
+}
--- main.go
+++ main.go
@@ -5,39 +5,12 @@ import (
 	"fmt"
 	"os"
 	"sync"
-  "time"
-  "strings"
-  "net/http"
-  "github.com/PuerkitoBio/gocrawl"
-  "github.com/PuerkitoBio/goquery"
 )
 
 var (
 	config = flag.String("c", "./config.json", "Configuration file for crawler")
 	outDir = flag.String("o", "./output", "Output directory to save crawled data")
 )
-
-type CrawlerExtender struct {
-  gocrawl.DefaultExtender
-  Section string
-  outDir string
-}
-
-func (this *CrawlerExtender) Visit(ctx *gocrawl.URLContext, res *http.Response, doc * goquery.Document) (interface{}, bool) {
-  title := strings.TrimSpace(doc.Find("title").Text())
-
-  if doc.Find(this.Section).Length() == 0 {
-    return nil, true
-  }
-
-  f, _ := os.Create(this.outDir +"/" + title + ".txt")
-  defer f.Close()
-
-  body := doc.Find(this.Section).Text()
-  f.WriteString(body)
-
-  return nil, true
-}
 
 
 func main() {
@@ -62,20 +35,4 @@ func prepareOutputDir() {
 	}
 }
 
-
-func crawlSite(siteConfig SiteConfig,wg *sync.WaitGroup) {
- defer wg.Done()
- crawler := new(CrawlerExtender)
- crawler.Section = siteConfig.Section
- crawler.outDir = *outDir
-
- opts := gocrawl.NewOptions(crawler)
- opts.CrawlDelay = 1 * time.Second
-
- opts.MaxVisits = siteConfig.Depth
-
- // Create crawler and start at root of duckduckgo
- c := gocrawl.NewCrawlerWithOptions(opts)
- c.Run(siteConfig.Url)
-}
 
