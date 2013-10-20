a783e3f42e2fb0eb13a97fb5c5a53fe8b12c337c
Jijesh Mohan <jijeshmohan@gmail.com>
2013-10-20 12:51:26 +0530

    added flag for output dir

--- main.go
+++ main.go
@@ -3,14 +3,24 @@ package main
 import (
 "flag"
 "fmt"
+"os"
 )
 
 var(
   config = flag.String("c","./config.json","Configuration file for crawler")
+  outDir = flag.String("o","./output","Output directory to save crawled data")
 )
 
 func main(){
   flag.Parse()
   siteConfigs := GetConfig(*config)
+  prepareOutputDir()
   fmt.Printf("No of sites to crawl : %d\n", len(siteConfigs))
+}
+
+func prepareOutputDir(){
+  if err := os.MkdirAll(*outDir,os.ModePerm); err != nil {
+    fmt.Println("Unable to create output dir: " , err)
+    os.Exit(1)
+  }
 }
